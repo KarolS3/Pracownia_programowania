46286db86910556fa060312d5268763202137064
KarolS3 <124045109+KarolS3@users.noreply.github.com>
2025-12-06 17:37:58 +0100

    Delete 2.6.py

--- 2.6.py
+++
@@ -1,9 +0,0 @@
-cel = int(input("Podaj docelowy dystans"))
-pdys = int(input("Podaj poczatkowy dystans"))
-pwzrost = int(input("Podaj wzrost dystansu"))
-dys = pdys
-tyg = 0
-while dys <= cel:
-    tyg += 1
-    dys += dys * (pwzrost / 100)
-    print(tyg,"Tydzien: ", dys)
