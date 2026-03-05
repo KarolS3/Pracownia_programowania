f5bbb4905e44dae7d81762a54c88c779b0b58acf
KarolS3 <124045109+KarolS3@users.noreply.github.com>
2026-03-05 10:41:27 +0100

    Singleton Geleta

---
+++ InznSingleton/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Wzorce_Projektowe
+{
+ 
+    public class Szkola
+    {
+        private static Szkola _instance;
+
+ 
+        private Szkola()
+        {
+        }
+
+        public static Szkola getInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Szkola();
+            }
+
+            return _instance;
+        }
+
+
+        public void wyswietlNapis()
+        {
+            Console.WriteLine("Szkola w Pcimiu jest najlepsza na świecie");
+        }
+
+        public unsafe void wyswietlID()
+        {
+            TypedReference tr = __makeref(_instance);
+            IntPtr ptr = **(IntPtr**)(&tr);
+            Console.WriteLine("Adres obiektu w pamieci: " + ptr);
+        }
+    }
+
+    class Singleton_Test
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Rozpoczecie programu Singleton");
+            Szkola.getInstance().wyswietlID();
+            Szkola.getInstance().wyswietlNapis();
+            Console.WriteLine("_____________");
+
+            drugaFunkcja();
+
+            Console.WriteLine("_____________");
+        }
+
+        static void drugaFunkcja()
+        {
+            Console.WriteLine("Rozpoczecie drugiej funkcji");
+            Szkola.getInstance().wyswietlID();
+            Szkola.getInstance().wyswietlNapis();
+        }
+    }
+}
